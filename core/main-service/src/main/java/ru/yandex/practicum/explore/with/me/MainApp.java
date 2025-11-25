7b344e6883b4dffc6e4161c8d2e1d5272a50f6b3
Frosty-WinPC <al.korobko@yahoo.com>
2025-11-25 15:57:22 +0100

    fix: пути пакетов в scanBasePackages

--- core/main-service/src/main/java/ru/yandex/practicum/explore/with/me/MainApp.java
+++ core/main-service/src/main/java/ru/yandex/practicum/explore/with/me/MainApp.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.explore.with.me;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum.explore.with.me", "ru.practicum.stat"})
+@SpringBootApplication(scanBasePackages = {"ru.yandex.practicum.explore.with.me", "ru.yandex.practicum.stats"})
 public class MainApp {
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
