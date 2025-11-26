eca257c8f1ec3e4a06cfe46b426d992e1d5efc6a
Frosty-WinPC <al.korobko@yahoo.com>
2025-11-26 22:03:59 +0100

    fix: исправления для запуска UserApp.java. исправление портов БД микросервисов в docker-compose.yml

--- core/user-service/src/main/java/ru/yandex/practicum/user/UserApp.java
+++ core/user-service/src/main/java/ru/yandex/practicum/user/UserApp.java
@@ -2,10 +2,14 @@ package ru.yandex.practicum.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication(scanBasePackages = {"ru.yandex.practicum.user", "ru.yandex.practicum.interaction.api"})
 @ConfigurationPropertiesScan
+@EntityScan(basePackages = {
+        "ru.yandex.practicum.interaction.api.model.user"
+})
 public class UserApp {
     public static void main(String[] args) {
         SpringApplication.run(UserApp.class, args);
