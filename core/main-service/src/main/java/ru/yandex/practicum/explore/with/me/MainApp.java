c7503dfb01e1b1a41fd8332bf9a095e261df239b
Frosty-WinPC <al.korobko@yahoo.com>
2025-11-26 20:01:02 +0100

    add: базовая структура модулей user-service, request-service, базовые зависимости.

--- core/main-service/src/main/java/ru/yandex/practicum/explore/with/me/MainApp.java
+++ core/main-service/src/main/java/ru/yandex/practicum/explore/with/me/MainApp.java
@@ -10,4 +10,26 @@ public class MainApp {
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
     }
+    //todo comment, category & compilation вроде как по желанию, можно пока не выносить
+    //todo circuit breaker, если сервис недоступен - возвращает значение 0
+
+    //todo 1) user service
+    // перенести контроллер, репозиторий, сервис.
+    // копировать исключения, дтошки, приложение
+    // копировать схему, вырезать лишнее. Копировать application.yml, заменить имя
+    // настроить рут
+    // добавить в конфиг сервер конфиги
+
+    //todo дальше удалить внешние ключи на таблицу users
+    // везде, где использовался User -> меняем на Long id
+    // userRepository -> userClient в отдельном модуле, в клиенте необходимые методы через FeignClient
+
+    //todo в event был пользователь, теперь его не будет, формируем список айдишников пользователей
+    // потом проходимся и формируем userDto или что требуется
+
+    //todo 2) переносим request service
+
+    //todo 3) переносим какую-то свою функциональность
+
+    //todo 4) переименовываем main-service -> event-service
 }
