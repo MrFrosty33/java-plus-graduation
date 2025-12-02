5050bfc0b7de296275a0790cbdee58a47b38e2b3
Frosty-WinPC <al.korobko@yahoo.com>
2025-12-02 19:01:10 +0100

    rename: main-service -> event-service

--- core/user-service/src/main/java/ru/yandex/practicum/user/UserApp.java
+++ core/user-service/src/main/java/ru/yandex/practicum/user/UserApp.java
@@ -15,20 +15,8 @@ public class UserApp {
         SpringApplication.run(UserApp.class, args);
     }
     //todo comment, category & compilation вроде как по желанию, можно пока не выносить
-    //todo circuit breaker, если сервис недоступен - возвращает значение 0
-
-    //todo дальше удалить внешние ключи на таблицу users
-    // везде, где использовался User -> меняем на Long id
-    // userRepository -> userClient в отдельном модуле, в клиенте необходимые методы через FeignClient
-
-    //todo в event был пользователь, теперь его не будет, формируем список айдишников пользователей
-    // потом проходимся и формируем userDto или что требуется
-
-    //todo 2) переносим request service
 
     //todo 3) переносим какую-то свою функциональность
-
-    //todo 4) переименовываем main-service -> event-service
 
     //todo не забыть добавлять руты
 }
