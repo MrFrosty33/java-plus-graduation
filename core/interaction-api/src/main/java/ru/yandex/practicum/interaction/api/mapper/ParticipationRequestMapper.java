14822a4c862bad1330949b46e1044653fe989f79
Frosty-WinPC <al.korobko@yahoo.com>
2025-12-02 15:45:43 +0100

    fix: ParticipationRequestMapper

--- core/interaction-api/src/main/java/ru/yandex/practicum/interaction/api/mapper/ParticipationRequestMapper.java
+++ core/interaction-api/src/main/java/ru/yandex/practicum/interaction/api/mapper/ParticipationRequestMapper.java
@@ -8,7 +8,7 @@ import ru.yandex.practicum.interaction.api.model.request.ParticipationRequestDto
 @Mapper(componentModel = "spring")
 public interface ParticipationRequestMapper {
 
-    @Mapping(target = "event", source = "event.id")
+    @Mapping(target = "event", source = "eventId")
     @Mapping(target = "requester", source = "requesterId")
     ParticipationRequestDto toDto(ParticipationRequest entity);
 }
