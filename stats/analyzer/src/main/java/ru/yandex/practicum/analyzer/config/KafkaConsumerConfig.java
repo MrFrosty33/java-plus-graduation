b92ecb48b3444c27968bbc6c001ba5dd6d7781d0
Frosty-WinPC <al.korobko@yahoo.com>
2025-12-11 17:08:24 +0100

    add: класс конфигурации с бинами для KafkaConsumer-ов модуля analyzer

---
+++ stats/analyzer/src/main/java/ru/yandex/practicum/analyzer/config/KafkaConsumerConfig.java
@@ -0,0 +1,55 @@
+package ru.yandex.practicum.analyzer.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import ru.practicum.ewm.stats.avro.EventSimilarityAvro;
+import ru.practicum.ewm.stats.avro.UserActionAvro;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaConsumerConfig {
+    private final KafkaEventsSimilarityConsumerConfig eventsSimilarityConsumerConfig;
+    private final KafkaUserActionsConsumerConfig userActionsConsumerConfig;
+
+    @Bean
+    public ConsumerFactory<Void, UserActionAvro> userActionsConsumerFactory() {
+        // DefaultKafkaConsumerFactory требует Map<String, Object>, потому такой маппинг
+        Map<String, Object> configMap = new HashMap<>();
+        for (String name : userActionsConsumerConfig.getProperties().stringPropertyNames()) {
+            configMap.put(name, userActionsConsumerConfig.getProperties().getProperty(name));
+        }
+
+        return new DefaultKafkaConsumerFactory<>(configMap);
+    }
+
+    @Bean
+    public ConsumerFactory<Void, EventSimilarityAvro> eventSimilarityAvroConsumerFactory() {
+        Map<String, Object> configMap = new HashMap<>();
+        for (String name : eventsSimilarityConsumerConfig.getProperties().stringPropertyNames()) {
+            configMap.put(name, eventsSimilarityConsumerConfig.getProperties().getProperty(name));
+        }
+
+        return new DefaultKafkaConsumerFactory<>(configMap);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<Void, UserActionAvro> userActionKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<Void, UserActionAvro> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(userActionsConsumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<Void, EventSimilarityAvro> eventSimilarityKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<Void, EventSimilarityAvro> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(eventSimilarityAvroConsumerFactory());
+        return factory;
+    }
+}
