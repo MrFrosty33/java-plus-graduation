dd66584241eb137568e14b3cd764ea59e585346d
Frosty-WinPC <al.korobko@yahoo.com>
2025-12-16 14:54:34 +0100

    add: мапы для хранения в памяти веса взаимодействия пользователя с событием & суммы минимальных весов пары мероприятий. Вспомогательные методы для работы с мапой minWeightSum

--- stats/aggregator/src/main/java/ru/yandex/practicum/aggregator/service/AggregatorServiceKafka.java
+++ stats/aggregator/src/main/java/ru/yandex/practicum/aggregator/service/AggregatorServiceKafka.java
@@ -15,6 +15,9 @@ import ru.yandex.practicum.aggregator.config.KafkaEventsSimilarityProducerConfig
 import ru.yandex.practicum.aggregator.config.TopicConfig;
 import ru.yandex.practicum.aggregator.exception.JsonException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 @Service
 public class AggregatorServiceKafka {
@@ -23,6 +26,12 @@ public class AggregatorServiceKafka {
     private final JsonMapper jsonMapper;
     private final KafkaProducer<Void, SpecificRecordBase> eventsSimilarityProducer;
     private final TopicConfig topicConfig;
+
+    //            Map<EventId, Map<UserId, Weight>>, максимальный вес взаимодействия пользователя с мероприятием
+    private final Map<Long, Map<Long, Double>> maxWeight = new HashMap<>();
+    //            Map<EventIdA, Map<EventIdB, S_minWeight>>, сумма минимальных весов для пары мероприятий
+    private final Map<Long, Map<Long, Double>> minWeightSum = new HashMap<>();
+
 
     public AggregatorServiceKafka(JsonMapper jsonMapper,
                                   KafkaEventsSimilarityProducerConfig kafkaEventsSimilarityProducerConfig, TopicConfig topicConfig) {
@@ -42,6 +51,7 @@ public class AggregatorServiceKafka {
             // если очередное - обновить S_min(A, B), S_a, S_b. После вычислить коэфф схожести для каждой пары
             // где в паре A - текущее мероприятие, В - каждое из других мероприятий
             // рассчитанные значения упаковать в EventSimilarityAvro и sendAvro();
+
         } catch (Exception e) {
             log.warn("{}: exception in consumeUserActions(): ", className, e);
         }
@@ -60,5 +70,23 @@ public class AggregatorServiceKafka {
             log.warn("{}: error processing avroMessage to JSON: {}", className, e.getMessage());
             throw new JsonException("Error processing avroMessage to JSON");
         }
+    }
+
+    private void putMinWeightSum(long eventA, long eventB, double sum) {
+        long first = Math.min(eventA, eventB);
+        long second = Math.max(eventA, eventB);
+
+        minWeightSum
+                .computeIfAbsent(first, e -> new HashMap<>())
+                .put(second, sum);
+    }
+
+    private double getMinWeightSum(long eventA, long eventB) {
+        long first = Math.min(eventA, eventB);
+        long second = Math.max(eventA, eventB);
+
+        return minWeightSum
+                .computeIfAbsent(first, e -> new HashMap<>())
+                .getOrDefault(second, 0.0);
     }
 }
