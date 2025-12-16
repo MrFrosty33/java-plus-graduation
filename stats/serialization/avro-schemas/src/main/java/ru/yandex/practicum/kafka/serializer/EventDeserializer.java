2d4ef794515d8a7707080ba90f7dbbc50a1073e8
Frosty-WinPC <al.korobko@yahoo.com>
2025-12-16 15:55:09 +0100

    delete: удалён неиспользующийся Event.avdl

--- stats/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/serializer/EventDeserializer.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.kafka.serializer;
-
-import ru.practicum.ewm.stats.avro.EventAvro;
-
-public class EventDeserializer extends BaseAvroDeserializer<EventAvro> {
-    public EventDeserializer() {
-        super(EventAvro.getClassSchema());
-    }
-}
